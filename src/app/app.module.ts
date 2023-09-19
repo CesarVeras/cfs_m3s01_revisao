cec3f3fcbbd284c6d6ec774636039335887daac2
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-09-19 17:06:49 -0300

    Layout de autenticação e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AutenticacaoLayoutComponent } from './layouts/autenticacao-layout/autenticacao-layout.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AutenticacaoLayoutComponent,
+    CadastroComponent
   ],
   imports: [
-    BrowserModule,
-		ReactiveFormsModule
+		BrowserModule,
+		RouterModule,
+		ReactiveFormsModule,
+		AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
