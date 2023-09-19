d554a23f03a5f238a9682e85f42a2dbb4fe2cca8
Eduardo <eduardo@pop-os.localdomain>
2023-09-19 14:03:18 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cfs_ms3s01_revisao';
+}
